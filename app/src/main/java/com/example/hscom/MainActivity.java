927ebe6b81f76e423f538f6db062a88471f63776
jmlee2005jm <44526560+jmlee2005jm@users.noreply.github.com>
2019-10-26 09:44:44 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/hscom/MainActivity.java
+++ app/src/main/java/com/example/hscom/MainActivity.java
@@ -4,22 +4,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.os.Handler;
+import android.os.Message;
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.intentBtn2);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent;
-                intent = new Intent(MainActivity.this,hscom_list.class);
-                startActivity(intent);
+        Handler handler = new Handler(){
+            public void handleMessage (Message msg){
+                super.handleMessage(msg);
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
             }
-        });
+        };
+        handler.sendEmptyMessageDelayed(0,2500);
     }
 }

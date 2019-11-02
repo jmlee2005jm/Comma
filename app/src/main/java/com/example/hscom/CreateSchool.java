2312cd25079de3eacccd2d1de7949484860510a2
Jeongmin Lee <jmlee2005jm@gmail.com>
2019-11-02 10:56:52 +0900

    View complete?

--- app/src/main/java/com/example/hscom/CreateSchool.java
+++ app/src/main/java/com/example/hscom/CreateSchool.java
@@ -1,9 +1,8 @@
 package com.example.hscom;
 
-import android.icu.text.IDNA;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
